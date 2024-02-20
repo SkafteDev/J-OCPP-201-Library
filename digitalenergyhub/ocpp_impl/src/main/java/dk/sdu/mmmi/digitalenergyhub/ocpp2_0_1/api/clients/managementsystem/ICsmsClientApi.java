a236265379c30c2c9224191acb8f1bef26dd0279
Christian Skafte Beck Clausen <csbc@mmmi.sdu.dk>
2024-02-20 19:30:39 +0100

    Refactored the Charging Station Management Client interface.

--- digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/api/clients/managementsystem/ICsmsClientApi.java
+++ digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/api/clients/managementsystem/ICsmsClientApi.java
@@ -1,6 +1,8 @@
 package dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.clients.managementsystem;
 
-import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.clients.managementsystem.provisioning.ICsmsClientProvisioningApi;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.rpcframework.api.ICallMessage;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.rpcframework.api.ICallResultMessage;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.schemas.json.*;
 
 /**
  * This interface defines the Charging Station Management System Client API.
@@ -8,19 +10,45 @@ import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.clients.managementsystem.provi
  * The operations in this interface represents the requests that a Charging Station Management System can invoke on
  * a Charging Station.
  *
- * The interface consists of sub-interfaces, each mapping to a functional block in the OCPP 2.0.1 specification.
- *
- * For example:
- * Provisioning API
- * SmartCharging API
- * Authentication API
- * etc.
  */
 public interface ICsmsClientApi {
-
-    /**
-     * Returns the API for the provisioning subsystem.
-     * @return
-     */
-    ICsmsClientProvisioningApi getProvisioningAPI();
+    ICallResultMessage<CancelReservationResponse> sendCancelReservationRequest(ICallMessage<CancelReservationRequest> request);
+    ICallResultMessage<CertificateSignedResponse> sendCertificateSignedRequest(ICallMessage<CertificateSignedRequest> request);
+    ICallResultMessage<ChangeAvailabilityResponse> sendChangeAvailabilityRequest(ICallMessage<ChangeAvailabilityRequest> request);
+    ICallResultMessage<ClearCacheResponse> sendClearCacheRequest(ICallMessage<ClearCacheRequest> request);
+    ICallResultMessage<ClearChargingProfileResponse> sendClearChargingProfileRequest(ICallMessage<ClearChargingProfileRequest> request);
+    ICallResultMessage<ClearDisplayMessageResponse> sendClearDisplayMessageRequest(ICallMessage<ClearDisplayMessageRequest> request);
+    ICallResultMessage<ClearVariableMonitoringResponse> sendClearVariableMonitoringRequest(ICallMessage<ClearVariableMonitoringRequest> request);
+    ICallResultMessage<CostUpdatedResponse> sendCostUpdatedRequest(ICallMessage<CostUpdatedRequest> request);
+    ICallResultMessage<CustomerInformationResponse> sendCustomerInformationRequest(ICallMessage<CustomerInformationRequest> request);
+    ICallResultMessage<DeleteCertificateResponse> sendDeleteCertificateRequest(ICallMessage<DeleteCertificateRequest> request);
+    ICallResultMessage<GetBaseReportResponse> sendGetBaseReportRequest(ICallMessage<GetBaseReportRequest> request);
+    ICallResultMessage<GetChargingProfilesResponse> sendGetChargingProfilesRequest(ICallMessage<GetChargingProfilesRequest> request);
+    ICallResultMessage<GetCompositeScheduleResponse> sendGetCompositeScheduleRequest(ICallMessage<GetCompositeScheduleRequest> request);
+    ICallResultMessage<GetDisplayMessagesResponse> sendGetDisplayMessagesRequest(ICallMessage<GetDisplayMessagesRequest> request);
+    ICallResultMessage<GetInstalledCertificateIdsResponse> sendGetInstalledCertificateIdsRequest(ICallMessage<GetInstalledCertificateIdsRequest> request);
+    ICallResultMessage<GetLocalListVersionResponse> sendGetLocalListVersionRequest(ICallMessage<GetLocalListVersionRequest> request);
+    ICallResultMessage<GetLogResponse> sendGetLogRequest(ICallMessage<GetLogRequest> request);
+    ICallResultMessage<GetMonitoringReportResponse> sendGetMonitoringReportRequest(ICallMessage<GetMonitoringReportRequest> request);
+    ICallResultMessage<GetReportResponse> sendGetReportRequest(ICallMessage<GetReportRequest> request);
+    ICallResultMessage<GetTransactionStatusResponse> sendGetTransactionStatusRequest(ICallMessage<GetTransactionStatusRequest> request);
+    ICallResultMessage<GetVariablesResponse> sendGetVariablesRequest(ICallMessage<GetVariablesRequest> request);
+    ICallResultMessage<InstallCertificateResponse> sendInstallCertificateRequest(ICallMessage<InstallCertificateRequest> request);
+    ICallResultMessage<MeterValuesResponse> sendMeterValuesRequest(ICallMessage<MeterValuesRequest> request);
+    ICallResultMessage<RequestStartTransactionResponse> sendRequestStartTransactionRequest(ICallMessage<RequestStartTransactionRequest> request);
+    ICallResultMessage<RequestStopTransactionResponse> sendRequestStopTransactionRequest(ICallMessage<RequestStopTransactionRequest> request);
+    ICallResultMessage<ReserveNowResponse> sendReserveNowRequest(ICallMessage<ReserveNowRequest> request);
+    ICallResultMessage<ResetResponse> sendResetRequest(ICallMessage<ResetRequest> request);
+    ICallResultMessage<SendLocalListResponse> sendSendLocalListRequest(ICallMessage<SendLocalListRequest> request);
+    ICallResultMessage<SetChargingProfileResponse> sendSetChargingProfileRequest(ICallMessage<SetChargingProfileRequest> request);
+    ICallResultMessage<SetDisplayMessageResponse> sendSetDisplayMessageRequest(ICallMessage<SetDisplayMessageRequest> request);
+    ICallResultMessage<SetMonitoringBaseResponse> sendSetMonitoringBaseRequest(ICallMessage<SetMonitoringBaseRequest> request);
+    ICallResultMessage<SetMonitoringLevelResponse> sendSetMonitoringLevelRequest(ICallMessage<SetMonitoringLevelRequest> request);
+    ICallResultMessage<SetNetworkProfileResponse> sendSetNetworkProfileRequest(ICallMessage<SetNetworkProfileRequest> request);
+    ICallResultMessage<SetVariableMonitoringResponse> sendSetVariableMonitoringRequest(ICallMessage<SetVariableMonitoringRequest> request);
+    ICallResultMessage<SetVariablesResponse> sendSetVariablesRequest(ICallMessage<SetVariablesRequest> request);
+    ICallResultMessage<TriggerMessageResponse> sendTriggerMessageRequest(ICallMessage<TriggerMessageRequest> request);
+    ICallResultMessage<UnlockConnectorResponse> sendUnlockConnectorRequest(ICallMessage<UnlockConnectorRequest> request);
+    ICallResultMessage<UnpublishFirmwareResponse> sendUnpublishFirmwareRequest(ICallMessage<UnpublishFirmwareRequest> request);
+    ICallResultMessage<UpdateFirmwareResponse> sendUpdateFirmwareRequest(ICallMessage<UpdateFirmwareRequest> request);
 }
--- digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/impl/clients/managementsystem/CsmsClientImpl.java
+++ digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/impl/clients/managementsystem/CsmsClientImpl.java
@@ -1,18 +1,225 @@
 package dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.impl.clients.managementsystem;
 
 import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.clients.managementsystem.ICsmsClientApi;
-import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.clients.managementsystem.provisioning.ICsmsClientProvisioningApi;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.rpcframework.api.ICallMessage;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.rpcframework.api.ICallResultMessage;
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.schemas.json.*;
+import io.nats.client.Connection;
 
 public class CsmsClientImpl implements ICsmsClientApi {
 
-    private final ICsmsClientProvisioningApi provisioningApi;
+    private final Connection natsConnection;
 
-    public CsmsClientImpl(ICsmsClientProvisioningApi provisioningApi) {
-        this.provisioningApi = provisioningApi;
+    public CsmsClientImpl(Connection natsConnection) {
+        this.natsConnection = natsConnection;
     }
 
     @Override
-    public ICsmsClientProvisioningApi getProvisioningAPI() {
-        return provisioningApi;
+    public ICallResultMessage<CancelReservationResponse> sendCancelReservationRequest(ICallMessage<CancelReservationRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<CertificateSignedResponse> sendCertificateSignedRequest(ICallMessage<CertificateSignedRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ChangeAvailabilityResponse> sendChangeAvailabilityRequest(ICallMessage<ChangeAvailabilityRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ClearCacheResponse> sendClearCacheRequest(ICallMessage<ClearCacheRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ClearChargingProfileResponse> sendClearChargingProfileRequest(ICallMessage<ClearChargingProfileRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ClearDisplayMessageResponse> sendClearDisplayMessageRequest(ICallMessage<ClearDisplayMessageRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ClearVariableMonitoringResponse> sendClearVariableMonitoringRequest(ICallMessage<ClearVariableMonitoringRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<CostUpdatedResponse> sendCostUpdatedRequest(ICallMessage<CostUpdatedRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<CustomerInformationResponse> sendCustomerInformationRequest(ICallMessage<CustomerInformationRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<DeleteCertificateResponse> sendDeleteCertificateRequest(ICallMessage<DeleteCertificateRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetBaseReportResponse> sendGetBaseReportRequest(ICallMessage<GetBaseReportRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetChargingProfilesResponse> sendGetChargingProfilesRequest(ICallMessage<GetChargingProfilesRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetCompositeScheduleResponse> sendGetCompositeScheduleRequest(ICallMessage<GetCompositeScheduleRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetDisplayMessagesResponse> sendGetDisplayMessagesRequest(ICallMessage<GetDisplayMessagesRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetInstalledCertificateIdsResponse> sendGetInstalledCertificateIdsRequest(ICallMessage<GetInstalledCertificateIdsRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetLocalListVersionResponse> sendGetLocalListVersionRequest(ICallMessage<GetLocalListVersionRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetLogResponse> sendGetLogRequest(ICallMessage<GetLogRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetMonitoringReportResponse> sendGetMonitoringReportRequest(ICallMessage<GetMonitoringReportRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetReportResponse> sendGetReportRequest(ICallMessage<GetReportRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetTransactionStatusResponse> sendGetTransactionStatusRequest(ICallMessage<GetTransactionStatusRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<GetVariablesResponse> sendGetVariablesRequest(ICallMessage<GetVariablesRequest> req) {
+        // ICallMessage<GetVariablesRequest> callMsg = CallMessageImpl.<GetVariablesRequest>newBuilder().build();
+
+        // Starting here:
+
+        // Serialize to JSON format.
+        // Optionally, validate format before sending.
+
+        // Send the msg to the NATS.io broker as a request with a pre-programmed timeout.
+
+        // Await a response from NATS.io.
+
+        // Deserialize the response from NATS.io.
+
+        // Return the response...
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<InstallCertificateResponse> sendInstallCertificateRequest(ICallMessage<InstallCertificateRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<MeterValuesResponse> sendMeterValuesRequest(ICallMessage<MeterValuesRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<RequestStartTransactionResponse> sendRequestStartTransactionRequest(ICallMessage<RequestStartTransactionRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<RequestStopTransactionResponse> sendRequestStopTransactionRequest(ICallMessage<RequestStopTransactionRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ReserveNowResponse> sendReserveNowRequest(ICallMessage<ReserveNowRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<ResetResponse> sendResetRequest(ICallMessage<ResetRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SendLocalListResponse> sendSendLocalListRequest(ICallMessage<SendLocalListRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetChargingProfileResponse> sendSetChargingProfileRequest(ICallMessage<SetChargingProfileRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetDisplayMessageResponse> sendSetDisplayMessageRequest(ICallMessage<SetDisplayMessageRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetMonitoringBaseResponse> sendSetMonitoringBaseRequest(ICallMessage<SetMonitoringBaseRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetMonitoringLevelResponse> sendSetMonitoringLevelRequest(ICallMessage<SetMonitoringLevelRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetNetworkProfileResponse> sendSetNetworkProfileRequest(ICallMessage<SetNetworkProfileRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetVariableMonitoringResponse> sendSetVariableMonitoringRequest(ICallMessage<SetVariableMonitoringRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<SetVariablesResponse> sendSetVariablesRequest(ICallMessage<SetVariablesRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<TriggerMessageResponse> sendTriggerMessageRequest(ICallMessage<TriggerMessageRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<UnlockConnectorResponse> sendUnlockConnectorRequest(ICallMessage<UnlockConnectorRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<UnpublishFirmwareResponse> sendUnpublishFirmwareRequest(ICallMessage<UnpublishFirmwareRequest> request) {
+        return null;
+    }
+
+    @Override
+    public ICallResultMessage<UpdateFirmwareResponse> sendUpdateFirmwareRequest(ICallMessage<UpdateFirmwareRequest> request) {
+        return null;
     }
 }
