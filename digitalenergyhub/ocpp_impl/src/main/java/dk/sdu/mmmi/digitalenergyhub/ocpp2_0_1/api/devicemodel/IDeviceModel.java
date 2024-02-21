be0066aff95234c2d7d3ec52b331dbb3b54e075b
Christian Clausen <csbc@mmmi.sdu.dk>
2024-02-21 20:53:46 +0100

    Create IDeviceModel.java

---
+++ digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/api/devicemodel/IDeviceModel.java
@@ -0,0 +1,38 @@
+package dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.devicemodel;
+
+import dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.schemas.json.VariableAttribute;
+
+/**
+ * This interface provides methods for accessing and modifying device model data.
+ */
+public interface IDeviceModel {
+
+    /**
+     * Gets the device model that represents the logical structure of a device, except for the VariableAttribute(s) of each Variable.
+     * @return A DeviceModelMap consisting of a (Component, VariableMap) pair.
+     * A VariableMap consists of a (Variable, VariableMetaData) pair.
+     * A VariableMetaData consists of a struct of 1 VariableCharacteristics and * VariableMonitoring.
+     */
+    //DeviceModelMap getDeviceModel();
+
+    //VariableAttribute getVariableAttribute(String componentId, String variableId, AttributeEnum attributeEnum);
+
+    /**
+     * Returns a list of VariableAttributes with maximum of 4 elements (Actual, Target, MinSet, MaxSet), and minimum size of 0 elements
+     * (if no VariableAttributes is present for the requested parameter).
+     * @param componentId
+     * @param variableId
+     * @return
+     */
+    //List<VariableAttribute> getVariableAttributes(String componentId, String variableId);
+
+    /**
+     * Sets the value of a VariableAttribute if present.
+     * @param componentId
+     * @param variableId
+     * @param attributeEnum
+     * @param value
+     * @return
+     */
+    //boolean setVariableAttributeValue(String componentId, String variableId, AttributeEnum attributeEnum, String value);
+}
