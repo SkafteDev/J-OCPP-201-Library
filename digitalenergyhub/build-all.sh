1aaf30d4460aae22e97ba126a005a032d0a207f0
Christian Clausen <csbc@mmmi.sdu.dk>
2024-02-27 12:45:29 +0100

    Update build-all.sh

--- digitalenergyhub/build-all.sh
+++ digitalenergyhub/build-all.sh
@@ -1,10 +1,12 @@
 #/bin/bash
 
-# Build the POJO's from OCPP 2.0.1 JSON schemas
-# NB! org.jsonschema2pojo.exception.ClassAlreadyExistsException is considered normal execution.
-# Link: https://github.com/joelittlejohn/jsonschema2pojo/issues/1555
-mvn clean install -pl ocpp_schemas
-
-
-# Build library files (JAR files) for the OCPP 2.0.1 implementation.
-mvn clean install -pl ocpp_impl -DskipTests
+# Build all modules within this Maven project.
+#
+#   ocpp_schemas:
+#     Build the POJO's from OCPP 2.0.1 JSON schemas
+#     NB! org.jsonschema2pojo.exception.ClassAlreadyExistsException is considered normal execution.
+#     Link: https://github.com/joelittlejohn/jsonschema2pojo/issues/1555
+#
+#   ocpp_impl:
+#     Build library files (JAR files) for the OCPP 2.0.1 implementation.
+mvn clean install -DskipTests
