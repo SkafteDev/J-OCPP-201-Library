6969e247d6232a8ae951206488602ccc1bc2107e
Christian Skafte Beck Clausen <csbc@mmmi.sdu.dk>
2024-02-28 18:22:39 +0100

    Update IDispatcher.java

--- digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/api/servers/dispatching/IDispatcher.java
+++ digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/api/servers/dispatching/IDispatcher.java
@@ -1,7 +1,7 @@
 package dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.api.servers.dispatching;
 
 /**
- * This interface represents a dispatcher that can receive CallMessages and handle them accordingly.
+ * This interface describes how to register/unregister a dispatcher.
  * @param <C> The type of the connection that is used to register the dispatcher.
  * @param <D> The type of the dispatcher.
  */
