d512d7f48027491b02a150ff7aadd9d72eea569e
Christian Skafte Beck Clausen <csbc@mmmi.sdu.dk>
2024-07-09 14:38:07 +0200

    Simplified the DateUtil class to only contain classes that are intended to be used by its clients. Added a corresponding test.

--- digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/impl/utils/DateUtil.java
+++ digitalenergyhub/ocpp_impl/src/main/java/dk/sdu/mmmi/digitalenergyhub/ocpp2_0_1/impl/utils/DateUtil.java
@@ -1,6 +1,7 @@
 package dk.sdu.mmmi.digitalenergyhub.ocpp2_0_1.impl.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Date;
 
@@ -9,39 +10,23 @@ public class DateUtil {
     private DateUtil() {}
 
     /**
-     * Returns a new Date object with a default ZoneOffset = UTC.
+     * Returns the UTC time encapsulated as a Date object.
      * @return
      */
     public static Date now() {
-        return now(ZoneOffset.UTC);
-    }
-
-    /**
-     * Returns a new Date object with the given ZoneOffset.
-     * @return
-     */
-    public static Date now(ZoneOffset offSet) {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        long epochMilliseconds = localDateTime.toEpochSecond(offSet) * 1000;
+        LocalDateTime localDateTime = LocalDateTime.now(ZoneId.of("UTC"));
+        long epochMilliseconds = localDateTime.toEpochSecond(ZoneOffset.UTC) * 1000;
 
         return new Date(epochMilliseconds);
     }
 
     /**
-     * Creates a new Date object with a default ZoneOffset = UTC.
+     * Returns the UTC time encapsulated as a Date object.
      * @return
      */
     public static Date of(int year, int month, int day, int hour, int minute, int second) {
-        return of(year, month, day, hour, minute, second, ZoneOffset.UTC);
-    }
-
-    /**
-     * Creates a new Date object with the given ZoneOffset.
-     * @return
-     */
-    public static Date of(int year, int month, int day, int hour, int minute, int second, ZoneOffset offSet) {
         LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour, minute, second);
-        long epochMilliseconds = localDateTime.toEpochSecond(offSet) * 1000;
+        long epochMilliseconds = localDateTime.toEpochSecond(ZoneOffset.UTC) * 1000;
 
         return new Date(epochMilliseconds);
     }
