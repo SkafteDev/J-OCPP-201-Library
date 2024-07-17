373c663d3750d08f2045c263043bfbee9515cbf2
Christian Clausen <csbc@mmmi.sdu.dk>
2024-07-17 14:52:01 +0200

    Added a custom serializer for the ZonedDateTime class to ensure that milliseconds are not serialized.

--- j-ocpp/j-ocpp-core/src/main/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/rpcframework/util/JacksonUtil.java
+++ j-ocpp/j-ocpp-core/src/main/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/rpcframework/util/JacksonUtil.java
@@ -1,22 +1,50 @@
 package dk.sdu.mmmi.jocpp.ocpp2_0_1.rpcframework.util;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class JacksonUtil {
     private JacksonUtil() {
     }
 
     public static ObjectMapper getDefault() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(ZonedDateTime.class, new CustomZonedDateTimeSerializer());
+
         ObjectMapper mapper = JsonMapper.builder()
-                .addModule(new JavaTimeModule()) // Needed to automatically register the com.fasterxml.jackson.datatype:jackson-datatype-jsr310 module.
+                .addModule(javaTimeModule) // Needed to automatically register the com.fasterxml.jackson.datatype:jackson-datatype-jsr310 module.
                 .enable(StreamReadFeature.INCLUDE_SOURCE_IN_LOCATION)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .build();
-
         return mapper;
+    }
+
+    /**
+     * Custom formatter to ensure RFC3339 compatibility.
+     * E.g.:
+     * '2024-07-17T12:54:48+02:00'  for UTC+2
+     * '2024-07-17T10:54:48Z'       for UTC+0
+     */
+    private static class CustomZonedDateTimeSerializer extends StdSerializer<ZonedDateTime> {
+        private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+
+        protected CustomZonedDateTimeSerializer() {
+            super(ZonedDateTime.class);
+        }
+
+        @Override
+        public void serialize(ZonedDateTime zonedDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeString(zonedDateTime.format(formatter));
+        }
     }
 }
--- j-ocpp/j-ocpp-core/src/test/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/unit/rpcframework/serialization/callresultmessage/payloads/BootNotificationResponseSerializerTest.java
+++ j-ocpp/j-ocpp-core/src/test/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/unit/rpcframework/serialization/callresultmessage/payloads/BootNotificationResponseSerializerTest.java
@@ -8,9 +8,7 @@ import dk.sdu.mmmi.jocpp.ocpp2_0_1.schemas.json.BootNotificationResponse;
 import dk.sdu.mmmi.jocpp.ocpp2_0_1.schemas.json.RegistrationStatusEnum;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -34,6 +32,40 @@ public class BootNotificationResponseSerializerTest {
             String expected = "[3,\"19223201\",{\"currentTime\":\"2013-02-01T20:53:32Z\",\"interval\":300,\"status\":\"Accepted\"}]";
 
             String actual = CallResultSerializer.serialize(callResultMessage);
+
+            assertEquals(expected, actual);
+        } catch (JsonProcessingException cause) {
+            fail("Failed to deserialize.", cause);
+        }
+    }
+
+    @Test
+    void unit_serialize_CallResultMessage_of_type_BootNotificationResponse_systemClockNow() {
+        // Step 1: Generate a fixed 'now' clock.
+        String dateString = "2024-07-17T12:54:48.106352+02:00";
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString);
+        Instant instant = offsetDateTime.toInstant();
+        Clock fixedClock = Clock.fixed(instant, ZoneId.of("Europe/Copenhagen"));
+        ZonedDateTime now = ZonedDateTime.now(fixedClock);
+
+        // Step 2: Construct the response.
+        BootNotificationResponse bootNotificationResponse = BootNotificationResponse.builder()
+                .withStatus(RegistrationStatusEnum.ACCEPTED)
+                .withCurrentTime(now)
+                .withInterval((int) Duration.ofMinutes(5).toSeconds())
+                .build();
+
+        ICallResult<BootNotificationResponse> callResult =
+                CallResultImpl.<BootNotificationResponse>newBuilder()
+                        .withMessageId("19223201")
+                        .withPayLoad(bootNotificationResponse)
+                        .build();
+
+        // Step 3: Test that the response was parsed as expected.
+        try {
+            String expected = "[3,\"19223201\",{\"currentTime\":\"2024-07-17T12:54:48+02:00\",\"interval\":300,\"status\":\"Accepted\"}]";
+
+            String actual = CallResultSerializer.serialize(callResult);
 
             assertEquals(expected, actual);
         } catch (JsonProcessingException cause) {
