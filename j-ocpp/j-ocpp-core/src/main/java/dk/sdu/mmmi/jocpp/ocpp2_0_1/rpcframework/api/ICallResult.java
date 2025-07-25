5b09a769f0f85e4bb123ebe4aeb9b056ab530b2d
Christian <mail@skafte.me>
2025-07-25 13:03:52 +0000

    Remove obsolete copyright headers

--- j-ocpp/j-ocpp-core/src/main/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/rpcframework/api/ICallResult.java
+++ j-ocpp/j-ocpp-core/src/main/java/dk/sdu/mmmi/jocpp/ocpp2_0_1/rpcframework/api/ICallResult.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2024 SDU Center for Energy Informatics, University of Southern Denmark.
- * All rights reserved.
- *
- * Author: Christian Skafte Beck Clausen (csbc@mmmi.sdu.dk)
- *
- *  This code is proprietary and confidential.
- *  Unauthorized copying of this file, via any medium, is strictly prohibited unless permission
- *  is granted by SDU Center for Energy Informatics, University of Southern Denmark.
- */
 
 package dk.sdu.mmmi.jocpp.ocpp2_0_1.rpcframework.api;
 
